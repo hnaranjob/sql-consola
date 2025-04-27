a8400c0bd1e5cab89a42f00fe02f56ba8f4f4e11
hnaranjob <61506999+hnaranjob@users.noreply.github.com>
2025-04-27 20:29:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -25,19 +25,19 @@ st.write("""
     Las principales funcionalidades de esta web son:
     
     - **Generar datos falsos** sobre clientes, empresas y consumos eléctricos. Puedes elegir el número de clientes y empresas y realizar consultas SQL sobre estos datos.
-    - **Subir un archivo CSV propio** con los datos que quieras consultar. Solo se permite archivos CSV y podrás realizar consultas SQL sobre esos datos.
+    - **Subir un archivo CSV, Excel (XLSX) o Excel con Macros (XLSM)** con los datos que quieras consultar. Podrás realizar consultas SQL sobre esos datos.
 
     **Lo más importante de esta web es que te proporciona una herramienta para empezar a practicar SQL de manera sencilla y accesible**, sin necesidad de instalar nada en tu ordenador. La idea es que puedas realizar consultas SQL sobre datos, aprender y experimentar.
 
     Las opciones que puedes elegir son:
     1. Generar datos falsos.
-    2. Subir un archivo CSV propio.
+    2. Subir un archivo (CSV, Excel o Excel con Macros).
 
     Para empezar, elige una de las opciones a continuación:
 """)
 
 # Opción para elegir entre generar datos falsos o subir un archivo
-opcion = st.radio("¿Qué deseas hacer?", ("Generar datos falsos", "Subir archivo CSV"))
+opcion = st.radio("¿Qué deseas hacer?", ("Generar datos falsos", "Subir archivo (CSV, Excel o XLSM)"))
 
 if opcion == "Generar datos falsos":
     # Selección de la semilla para generar datos replicables
@@ -102,18 +102,21 @@ if opcion == "Generar datos falsos":
     con.register('empresas_tarifas', df_empresas_tarifas)
     con.register('clientes_consumo', df_clientes_consumo)
 
-elif opcion == "Subir archivo CSV":
-    # Opción de subir un archivo CSV
-    uploaded_file = st.file_uploader("Sube tu archivo CSV", type="csv")
+elif opcion == "Subir archivo (CSV, Excel o XLSM)":
+    # Opción de subir un archivo (CSV, Excel o Excel con macros)
+    uploaded_file = st.file_uploader("Sube tu archivo (CSV, Excel o XLSM)", type=["csv", "xlsx", "xlsm"])
 
     if uploaded_file is not None:
         try:
-            # Cargar el CSV
-            df_subido = pd.read_csv(uploaded_file)
+            # Intentamos leer el archivo en función de su extensión
+            if uploaded_file.name.endswith(".csv"):
+                df_subido = pd.read_csv(uploaded_file)  # Leer CSV
+            else:
+                df_subido = pd.read_excel(uploaded_file, engine='openpyxl')  # Leer Excel o XLSM
 
             # Verificar si el DataFrame se cargó correctamente
             st.write("### Muestra de los datos subidos:")
-            st.dataframe(df_subido.head())  # Mostrar solo las primeras filas del CSV
+            st.dataframe(df_subido.head())  # Mostrar solo las primeras filas del archivo
 
             # Crear una base de datos temporal de DuckDB
             con = duckdb.connect(database=':memory:')
